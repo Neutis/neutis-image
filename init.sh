f001ae38b2246b88ac2493b139c224693a7ada3d
AD-Aleksandrov <aleksandr.aleksandrov@emlid.com>
2018-09-20 16:49:14 +0300

    Mender support & sumo release
    - add distro flag for init.sh
    - mender support
    - move to sumo yocto release

--- init.sh
+++ init.sh
@@ -1,3 +1,27 @@
+#!/bin/bash
+
+IMAGE_DISTRO="poky-neutis"
+
+while (( "$#" )); do
+  case "$1" in
+    -d|--distro)
+      IMAGE_DISTRO=$2
+      shift 2
+      ;;
+    --) # end argument parsing
+      shift
+      break
+      ;;
+    -*|--*=) # unsupported flags
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+    *) # preserve positional arguments
+      shift
+      ;;
+  esac
+done
+
 # Update submodules
 git submodule update --init --recursive
 
@@ -19,10 +43,22 @@ meta_layers=(
 	'meta-emlid-neutis-examples'
 )
 
+if [ $IMAGE_DISTRO == "poky-neutis-mender" ]; then
+    meta_layers=(
+        ${meta_layers[@]}
+        'meta-mender/meta-mender-core'
+        'meta-mender/meta-mender-neutis'
+    )
+fi
+
 for i in ${meta_layers[@]}; do
 	bitbake-layers add-layer ../meta-layers/${i}
 done
 
 # Copy conf file
-cp ../conf/local.conf conf/ 
+cp ../conf/local.conf conf/local.conf
+
+if [ $IMAGE_DISTRO == "poky-neutis-mender" ]; then
+    sed -i '/DISTRO ?=/c\DISTRO ?= "'$IMAGE_DISTRO\" conf/local.conf
+fi
 
